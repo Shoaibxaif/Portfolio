562ce9b620591827e4da88a72efcbff72c6d06b7
mohammad shoaib <mohammadshoaib7221@gmail.com>
2023-10-26 23:44:02 +0530

    footer updated

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,27 +1,19 @@
-import { Box, Stack, Text, VStack } from '@chakra-ui/react'
+import { Box, Stack, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Footer = () => {
   return (
     <Box
-    bgColor={"blackAlpha.900"}
-    color={"whiteAlpha.700"}
-    minH={"48"}
+    bgColor={"#607B96"}
+    minH={"20"}
     px={"16"}
     py={["16", "8"]}
   >
-    <Stack direction={["column", "row"]} alignItems={"center"}>
-      <VStack w={"full"}>
-        <Text fontWeight={"bold"}>About Us</Text>
-        <Text
-          fontSize={"sm"}
-          letterSpacing={"widest"}
-          textAlign={["center", "left"]}
-        >
-          We are the best crypto trading app in India, we provide our guidance
-          at a very cheap price.
-        </Text>
-      </VStack>
+    <Stack  alignItems={"center"}>
+      
+        <Text fontWeight={"bold"}>Made By  Mohammad Shoaib</Text>
+
+      
     </Stack>
   </Box>
   )
